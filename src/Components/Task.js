196d93c2b23eaf2b2293caee750f9fcc17513601
cullnan <58110495+cullnan@users.noreply.github.com>
2022-06-16 19:13:47 +0600

    Update Task.js

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,4 +1,4 @@
-import React,  {useState, useContext} from 'react'
+import React,  { useContext} from 'react'
 import { Checkbox } from 'antd';
 import { DeleteOutlined } from '@ant-design/icons';
 import { Context } from '../context';
